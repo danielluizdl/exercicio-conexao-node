d207f1491782919767bebd4c1a18abb7d8482f97
Daniel Luiz <loureirodlg@gmail.com>
2024-04-16 20:16:28 -0300

    configurando a conexao com pg-pool

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require('express');
-const { Client } = require('pg');
-const { client } = require('pg');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '1343126',
+    database: 'aula_conexao_node_pg'
+});
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    const client = new Client({
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: '1343126',
-        database: 'aula_conexao_node_pg'
-    })
-
     try {
-        await client.connect()
-
-        const resultado = await client.query('select * from empresas')
-
-        await client.end()
-
-        return res.json(resultado.rows)
+        const resultado = await pool.query('select * from empresas')
+        return res.json(resultado)
     } catch (error) {
         console.log(error.message);
     }
