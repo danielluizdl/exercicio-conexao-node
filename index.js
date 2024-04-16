888ce78747b0fc018694a206cd0a0817ffec4bb4
Daniel Luiz <loureirodlg@gmail.com>
2024-04-16 19:56:23 -0300

    configurando usando pg-client

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const { Client } = require('pg');
+const { client } = require('pg');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json('Tudo Certo')
+app.get('/', async (req, res) => {
+    const client = new Client({
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: '1343126',
+        database: 'aula_conexao_node_pg'
+    })
+
+    try {
+        await client.connect()
+
+        const resultado = await client.query('select * from empresas')
+
+        await client.end()
+
+        return res.json(resultado.rows)
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 app.listen(3000)
