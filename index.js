11bda9f8c4e72f586845936b02e15d75af918d0d
Daniel Luiz <loureirodlg@gmail.com>
2024-04-16 21:10:30 -0300

    usando pg-pool para passar valores dinamicos na query

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '1343126',
-    database: 'aula_conexao_node_pg'
-});
+const pool = require('./conexao')
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/:id', async (req, res) => {
+    const { id } = req.params
     try {
-        const resultado = await pool.query('select * from empresas')
-        return res.json(resultado)
+        // const query = 'select * from empresas where nome = $1 or nome = $2'
+        // const params = ['Google', 'Facebook']
+
+        const query = 'update empresas set site = $1 where id = $2'
+        const params = ['www.cakewalk.com', 1]
+
+        const resultado = await pool.query(query, params)
+        return res.json(resultado.rows)
     } catch (error) {
         console.log(error.message);
     }
